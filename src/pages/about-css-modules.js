ab43f092fecf4969b5f97f064b72ae296ed6c8e8
Rory Murray <rory.christian.murray@gmail.com>
2019-09-09 21:04:27 -0400

    Add CSS module to about-css-modules

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Container from "../components/container"
+import styles from "./about-css-modules.module.css"
+console.log(styles)
 
 export default () => (
   <Container>
