0753f191748fe0b0e25c00a7e223a1fcdf8a46eb
Rory Murray <rory.christian.murray@gmail.com>
2019-09-09 20:54:46 -0400

    Create CSS modules page

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Container from "../components/container"
+
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS modules are cool</p>
+  </Container>
+)
