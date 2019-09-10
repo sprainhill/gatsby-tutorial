038f8a81ae7691b9556960ae3c86b70dc58aff41
Rory Murray <rory.christian.murray@gmail.com>
2019-09-10 09:59:45 -0400

    Finish tutorial pt 2

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -17,5 +17,15 @@ export default () => (
   <Container>
     <h1>About CSS Modules</h1>
     <p>CSS modules are cool</p>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
   </Container>
 )
