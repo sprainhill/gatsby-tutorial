8c7a47027badbd8ef5690cfc5239c0dd953d0806
Rory Murray <rory.christian.murray@gmail.com>
2019-09-09 21:12:38 -0400

    Create User

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -3,6 +3,16 @@ import Container from "../components/container"
 import styles from "./about-css-modules.module.css"
 console.log(styles)
 
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.usersname}</h2>
+      <p classname={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
 export default () => (
   <Container>
     <h1>About CSS Modules</h1>
